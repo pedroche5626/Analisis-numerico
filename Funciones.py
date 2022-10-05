7accac4291335d9fbf6dd8b6c4394aa0d45ff9c3
pedroche5626 <115035620+pedroche5626@users.noreply.github.com>
2022-10-05 12:26:31 -0700

    Create Funciones.py

---
+++ Funciones.py
@@ -0,0 +1,35 @@
+import math
+
+x=30
+
+a = math.cos(x)
+
+print(a)
+
+#sin(x)
+a = math.sin(x)
+
+print(a)
+
+#tan(x)
+a = math.tan(x)
+print(a)
+
+#raiz_cuadrada(4)
+a = math.sqrt(4)
+
+print(a)
+
+#2^2
+a = math.pow(2,2)
+
+print(a)
+
+
+a = math.pi
+
+print(a)
+
+a = math.e
+
+print(a)
